b222a1b0a4ff81d1909ff6352ac854e1be887f23
BSOD-qian <3348640434@qq.com>
2022-01-08 18:15:45 +0800

    1

--- sw.js
+++ sw.js
@@ -1,5 +1,15 @@
+const staticAssets = [
+    './',
+    './article',
+    './logo.ico'
+];
+
+self.addEventListener('install', async event => {
+    const cache = await caches.open('static-meme');
+    cache.addAll(staticAssets);
+});
+
 self.addEventListener('fetch', event => {
-    //caching for offline viewing
     const {request} = event;
     const url = new URL(request.url);
     if(url.origin === location.origin) {
@@ -7,8 +17,24 @@ self.addEventListener('fetch', event => {
     } else {
         event.respondWith(networkFirst(request));
     }
+
 });
+
 async function cacheData(request) {
     const cachedResponse = await caches.match(request);
     return cachedResponse || fetch(request);
+}
+
+async function networkFirst(request) {
+    const cache = await caches.open('dynamic-meme');
+
+    try {
+        const response = await fetch(request);
+        cache.put(request, response.clone());
+        return response;
+    } catch (error){
+        return await cache.match(request);
+
+    }
+
 }
